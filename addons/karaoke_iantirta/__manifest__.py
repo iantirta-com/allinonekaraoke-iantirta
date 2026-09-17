e4b551ebaebecb7fa8e33eb97a661b51a3a5710b
Yancovenant <tirtamoto@gmail.com>
2026-09-17 16:38:41 +0700

    init karoake

---
+++ addons/karaoke_iantirta/__manifest__.py
@@ -0,0 +1,16 @@
+{  # noqa: B018
+    'name': "web_iantirta",
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'description': """
+Long description of module's purpose
+    """,
+    'author': "iantirta.com",
+    'website': "https://www.iantirta.com.com",
+    'category': 'Hidden',
+    'version': '0.1',
+    "license": "Other OSI approved licence",
+    'depends': ['web_iantirta'],
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+}
